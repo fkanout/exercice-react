920ad602227510e5626928ef24f71141293b1674
Faisal KANOUT <f.kanout@gmail.com>
2017-05-16 10:47:41 +0200

    Comments ENV var

--- src/js/actions/hero.js
+++ src/js/actions/hero.js
@@ -3,7 +3,7 @@ import Constant from '../Constants';
 import $ from 'jquery';
 import crypto from 'crypto'
 
-
+// This is not so cool, should make these var as ENV variables when go production 
 const URL = 'http://gateway.marvel.com:80';
 const API_PUBLIC = '298bab46381a6daaaee19aa5c8cafea5';
 const API_PRIVATE = 'b0223681fced28de0fe97e6b9cd091dd36a5b71d';
